2276855cf8ef4ab467f3ef9c537428d7d3a2ac9c
ruben2daw <ruben2daw@gmail.com>
2016-11-26 14:25:11 +0100

    modicando autoloadRuta

--- lib/autoload.php
+++ lib/autoload.php
@@ -6,7 +6,7 @@ spl_autoload_register('application_autoloader');
 
 function application_autoloader($class) {
     $class_filename = $class.'.php';
-    $class_root = $_SERVER['DOCUMENT_ROOT'].'/ProyectoKarting/';
+    $class_root = $_SERVER['DOCUMENT_ROOT'].'/ProyectoFinal/ProyectoKarting';
 
     /* Determine the location of the file within the $class_root and, if found, load and cache it */
     $directories = new RecursiveDirectoryIterator($class_root);
