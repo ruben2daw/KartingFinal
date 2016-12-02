c6d52bdd27e5217a441c8fa08eee8d39f33cbd59
sergiodaw2 <sergio1994daw@gmail.com>
2016-12-02 11:31:56 +0100

    mejorTiempo.php con intento de consulta

--- templates/user/mejorTiempo.php
+++ templates/user/mejorTiempo.php
@@ -6,4 +6,24 @@
  * Time: 9:56
  */
 
-echo "mejor tiempo";
+
+
+//echo   $_SESSION['user'];
+class MejorTiempo {
+    public function bestTime()
+    {
+
+        $ds = new DataSource();
+        $sql = sprintf("SELECT kt.type, sl.time
+                       from kart_type as kt, session_laps as sl, karts as k, session_users as su, users as u where u.id=id ",
+            self::_TABLE); //EL WHERE ESTA MAL!!
+        $mejorTiempo = $ds->fetchAll($sql, self::_CLASS);
+        $ds->close();
+
+        return $mejorTiempo;
+
+    }
+}
+
+?>
+
